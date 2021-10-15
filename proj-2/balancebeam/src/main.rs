5be6abfddf2264415ad62ad79575b90ead0f1499
algebra2k <jackson.cloudnative@gmail.com>
2021-10-15 15:56:10 +0800

    proj-2: milestone2 done

--- proj-2/balancebeam/src/main.rs
+++ proj-2/balancebeam/src/main.rs
@@ -3,10 +3,13 @@ mod response;
 
 use clap::Clap;
 use rand::{Rng, SeedableRng};
-use std::net::{TcpListener, TcpStream};
-use std::sync::{Arc, Mutex};
-use threadpool::ThreadPool;
-
+// use std::net::{TcpListener, TcpStream};
+// use std::sync::{Arc, Mutex};
+// use threadpool::ThreadPool;
+// use std::net::{TcpListener, TcpStream};
+use std::sync::Arc;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex;
 /// Contains information parsed from the command-line invocation of balancebeam. The Clap macros
 /// provide a fancy way to automatically construct a command-line argument parser.
 #[derive(Clap, Debug)]
@@ -59,7 +62,8 @@ struct ProxyState {
     upstream_addresses: Vec<String>,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // Initialize the logging library. You can print log messages using the `log` macros:
     // https://docs.rs/log/0.4.8/log/ You are welcome to continue using print! statements; this
     // just looks a little prettier.
@@ -76,7 +80,7 @@ fn main() {
     }
 
     // Start listening for connections
-    let listener = match TcpListener::bind(&options.bind) {
+    let mut listener = match TcpListener::bind(&options.bind).await {
         Ok(listener) => listener,
         Err(err) => {
             log::error!("Could not bind to {}: {}", options.bind, err);
@@ -95,66 +99,88 @@ fn main() {
 
     let share_state: Arc<Mutex<ProxyState>> = Arc::new(Mutex::new(state));
 
-    let num_threads = num_cpus::get();
-    let thread_pool = ThreadPool::new(num_threads);
+    // let num_threads = num_cpus::get();
+    // let thread_pool = ThreadPool::new(num_threads);
 
-    for stream in listener.incoming() {
-        if let Ok(stream) = stream {
-            // Handle the connection!
-            // handle_connection(stream, &state);
-            dispatch_connection_handle(&thread_pool, stream, share_state.clone());
+    // for stream in listener.incoming() {
+    //     if let Ok(stream) = stream {
+    //         // Handle the connection!
+    //         // handle_connection(stream, &state);
+    //         dispatch_connection_handle(&thread_pool, stream, share_state.clone());
+    //     }
+    // }
+    loop {
+        match listener.accept().await {
+            Ok((stream, _sock_addr)) => {
+                // task::spawn(async );
+                dispatch_connection_handle(stream, share_state.clone()).await;
+            }
+            Err(e) => {
+                println!("couldn't get client: {:?}", e);
+            }
         }
     }
 }
 
-fn connect_to_upstream(state: &ProxyState) -> Result<TcpStream, std::io::Error> {
+async fn connect_to_upstream(state: &ProxyState) -> Result<TcpStream, std::io::Error> {
     let mut rng = rand::rngs::StdRng::from_entropy();
     let upstream_idx = rng.gen_range(0, state.upstream_addresses.len());
     let upstream_ip = &state.upstream_addresses[upstream_idx];
-    TcpStream::connect(upstream_ip).or_else(|err| {
+    TcpStream::connect(upstream_ip).await.or_else(|err| {
         log::error!("Failed to connect to upstream {}: {}", upstream_ip, err);
         Err(err)
     })
     // TODO: implement failover (milestone 3)
 }
 
-fn send_response(client_conn: &mut TcpStream, response: &http::Response<Vec<u8>>) {
+async fn send_response(client_conn: &mut TcpStream, response: &http::Response<Vec<u8>>) {
     let client_ip = client_conn.peer_addr().unwrap().ip().to_string();
     log::info!(
         "{} <- {}",
         client_ip,
         response::format_response_line(&response)
     );
-    if let Err(error) = response::write_to_stream(&response, client_conn) {
+    if let Err(error) = response::write_to_stream(&response, client_conn).await {
         log::warn!("Failed to send response to client: {}", error);
         return;
     }
 }
 
-fn dispatch_connection_handle(
-    thread_pool: &ThreadPool,
-    client_conn: TcpStream,
-    share_state: Arc<Mutex<ProxyState>>,
-) {
-    thread_pool.execute(move || {
+// fn dispatch_connection_handle(
+//     thread_pool: &ThreadPool,
+//     client_conn: TcpStream,
+//     share_state: Arc<Mutex<ProxyState>>,
+// ) {
+//     thread_pool.execute(move || {
+//         let state;
+//         {
+//             state = share_state.lock().unwrap();
+//         }
+//         handle_connection(client_conn, &state);
+//     })
+// }
+
+async fn dispatch_connection_handle(client_conn: TcpStream, share_state: Arc<Mutex<ProxyState>>) {
+    tokio::spawn(async move {
         let state;
         {
-            state = share_state.lock().unwrap();
+            state = share_state.lock().await;
         }
-        handle_connection(client_conn, &state);
+        handle_connection(client_conn, &state).await
     })
+    .await
+    .unwrap();
 }
-
-fn handle_connection(mut client_conn: TcpStream, state: &ProxyState) {
+async fn handle_connection(mut client_conn: TcpStream, state: &ProxyState) {
     let client_ip = client_conn.peer_addr().unwrap().ip().to_string();
     log::info!("Connection received from {}", client_ip);
 
     // Open a connection to a random destination server
-    let mut upstream_conn = match connect_to_upstream(state) {
+    let mut upstream_conn = match connect_to_upstream(state).await {
         Ok(stream) => stream,
         Err(_error) => {
             let response = response::make_http_error(http::StatusCode::BAD_GATEWAY);
-            send_response(&mut client_conn, &response);
+            send_response(&mut client_conn, &response).await;
             return;
         }
     };
@@ -164,7 +190,7 @@ fn handle_connection(mut client_conn: TcpStream, state: &ProxyState) {
     // client hangs up or we get an error.
     loop {
         // Read a request from the client
-        let mut request = match request::read_from_stream(&mut client_conn) {
+        let mut request = match request::read_from_stream(&mut client_conn).await {
             Ok(request) => request,
             // Handle case where client closed connection and is no longer sending requests
             Err(request::Error::IncompleteRequest(0)) => {
@@ -186,7 +212,7 @@ fn handle_connection(mut client_conn: TcpStream, state: &ProxyState) {
                     request::Error::RequestBodyTooLarge => http::StatusCode::PAYLOAD_TOO_LARGE,
                     request::Error::ConnectionError(_) => http::StatusCode::SERVICE_UNAVAILABLE,
                 });
-                send_response(&mut client_conn, &response);
+                send_response(&mut client_conn, &response).await;
                 continue;
             }
         };
@@ -203,30 +229,31 @@ fn handle_connection(mut client_conn: TcpStream, state: &ProxyState) {
         request::extend_header_value(&mut request, "x-forwarded-for", &client_ip);
 
         // Forward the request to the server
-        if let Err(error) = request::write_to_stream(&request, &mut upstream_conn) {
+        if let Err(error) = request::write_to_stream(&request, &mut upstream_conn).await {
             log::error!(
                 "Failed to send request to upstream {}: {}",
                 upstream_ip,
                 error
             );
             let response = response::make_http_error(http::StatusCode::BAD_GATEWAY);
-            send_response(&mut client_conn, &response);
+            send_response(&mut client_conn, &response).await;
             return;
         }
         log::debug!("Forwarded request to server");
 
         // Read the server's response
-        let response = match response::read_from_stream(&mut upstream_conn, request.method()) {
+        let response = match response::read_from_stream(&mut upstream_conn, request.method()).await
+        {
             Ok(response) => response,
             Err(error) => {
                 log::error!("Error reading response from server: {:?}", error);
                 let response = response::make_http_error(http::StatusCode::BAD_GATEWAY);
-                send_response(&mut client_conn, &response);
+                send_response(&mut client_conn, &response).await;
                 return;
             }
         };
         // Forward the response to the client
-        send_response(&mut client_conn, &response);
+        send_response(&mut client_conn, &response).await;
         log::debug!("Forwarded response to client");
     }
 }
